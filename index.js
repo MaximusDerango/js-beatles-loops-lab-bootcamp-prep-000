c698b3097e1df5cba0c465020dc62363fbe3de56
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 20:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var theBeatlesPlay = function(musicians, instruments) {
-  artist_instruments = [];
+  var artist_instruments = [];
   for (let i = 0; i < musicians.length; i++) {
     artist_instruments.push(`${musicians[i]} plays ${instruments[i]}`);
   }
